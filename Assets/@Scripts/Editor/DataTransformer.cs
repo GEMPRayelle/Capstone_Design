cf768b575653b110d8db3021a5565f669059ebcf
Rayelle <equation0703@naver.com>
2025-08-04 00:44:28 +0900

    feat : add Data Sheet
    스킬, 투사체용 데이터시트 추가 및 파싱 작업 완료

--- Assets/@Scripts/Editor/DataTransformer.cs
+++ Assets/@Scripts/Editor/DataTransformer.cs
@@ -18,6 +18,8 @@ public class DataTransformer : EditorWindow
     {
         ParseExcelDataToJson<HeroDataLoader, HeroData>("Hero");
         ParseExcelDataToJson<MonsterDataLoader, MonsterData>("Monster");
+        ParseExcelDataToJson<SkillDataLoader, SkillData>("Skill");
+        ParseExcelDataToJson<ProjectileDataLoader, ProjectileData>("Projectile");
 
         Debug.Log("DataTransformer Completed");
     }
--- Assets/@Scripts/Managers/Core/DataManager.cs
+++ Assets/@Scripts/Managers/Core/DataManager.cs
@@ -14,12 +14,16 @@ public class DataManager
     #region 각 데이터들의 Dictionary
     public Dictionary<int, Data.HeroData> HeroDic { get; private set; } = new Dictionary<int, Data.HeroData>();
     public Dictionary<int, Data.MonsterData> MonsterDic { get; private set; } = new Dictionary<int, Data.MonsterData>();
+    public Dictionary<int, Data.SkillData> SkillDic { get; private set; } = new Dictionary<int, Data.SkillData>();
+    public Dictionary<int, Data.ProjectileData> ProjectileDic { get; private set; } = new Dictionary<int, Data.ProjectileData>();
     #endregion
 
     public void Init()
     {
         HeroDic = LoadJson<Data.HeroDataLoader, int, Data.HeroData>("HeroData").MakeDict();
         MonsterDic = LoadJson<Data.MonsterDataLoader, int, Data.MonsterData>("MonsterData").MakeDict();
+        SkillDic = LoadJson<Data.SkillDataLoader, int, Data.SkillData>("SkillData").MakeDict();
+        ProjectileDic = LoadJson<Data.ProjectileDataLoader, int, Data.ProjectileData>("ProjectileData").MakeDict();
     }
 
     /// <summary>
